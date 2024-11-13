c9def744365737f61a15f2fd2d64fbd3973f8015
adeelfeb <at4563323@gmail.com>
2024-11-13 18:19:52 +0500

    Add basic understanding of Mongoose schemas and models
    - Defined a basic Mongoose schema and model for the Todo collection
    - Schema: Defines the structure and rules for documents within a MongoDB collection. Each field in the schema specifies data type, optional requirements, uniqueness, and other constraints.
    - Model: A compiled version of the schema, providing an interface to interact with the MongoDB database. Models allow us to perform CRUD operations (Create, Read, Update, Delete) on collections using JavaScript code.
    
    Mongoose Flow:
    1. Define a schema that outlines the structure of the documents in a collection.
    2. Create a model based on the schema to interact with the collection.
    3. Use the model in the backend to perform operations (e.g., creating new documents, querying, updating, or deleting).
    4. Frontend (e.g., React) interacts with the backend, sending requests. The backend then uses Mongoose models to manage data stored in MongoDB.

--- models/todos/user.models.js
+++ models/todos/user.models.js
@@ -1,3 +1,25 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+
+}, {timeStamps: true})
+
+
+export const User = mongoose.model('User', userSchema)
