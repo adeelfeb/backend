d84655b0df298ed79d5cc76910af60d7891a0f35
adeelfeb <at4563323@gmail.com>
2024-11-17 18:32:01 +0500

    Implement user schema with authentication features: - **Password Hashing**: Added pre-save hook to hash passwords using bcrypt before saving to the database. Ensures password security by hashing sensitive data. - **Password Comparison**: Added method `isPasswrodCorrect` to compare plain text passwords with the hashed password stored in the database during login. - **Access Token Generation**: Created method `generateAccessToken` to generate JWTs for user authentication. Includes user details (ID, username, email, fullname) and an expiration time based on environment variables. - **Refresh Token Generation**: Added `generateRefreshToken` method to create refresh tokens, used for token renewal. Refresh tokens have a longer expiry and are signed with a different secret. - **Model Export**: Exported the `User` model to interact with MongoDB for creating, updating, and querying user data.

--- learning/models/user.model.js
+++ learning/models/user.model.js
@@ -49,4 +49,38 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswrodCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  jwt.sign({
+    _id: this._id,
+    username: this.username,
+    email: this.email,
+    fullname: this.fullname
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+  jwt.sign({
+    _id: this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
+}
+
 export const User = mongoose.model("User", userSchema);
