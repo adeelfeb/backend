3a77da406b28d09e9d459a5be434f97651c8a7e9
adeelfeb <at4563323@gmail.com>
2024-11-19 00:29:53 +0500

    Check Point before the refreshAccessToken thing

--- learning/controllers/user.controller.js
+++ learning/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 
 const generateAccessAndRefreshToken = async (userId)=>{
@@ -187,6 +188,14 @@ const logoutUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, "User Logged Out"));
 });
 
+
+const refreshAccessToken = asyncHandler(async (req, res)=>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized Request")
+    }
+    
+})
 
 export { registerUser,
     loginUser,
